0280b116094100eb5afa0b259ccfa5367e881344
JYNoelChoi <noelchoi@hotmail.com>
2025-08-14 17:49:47 +0800

    create interests screen

--- lib/features/authentication/birthday_screen.dart
+++ lib/features/authentication/birthday_screen.dart
@@ -4,6 +4,7 @@ import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({super.key});
@@ -32,7 +33,7 @@ class _BirthdayScreenState extends State<BirthdayScreen> {
   void _onNextTap() {
     Navigator.of(
       context,
-    ).push(MaterialPageRoute(builder: (context) => EmailScreen()));
+    ).push(MaterialPageRoute(builder: (context) => InterestsScreen()));
   }
 
   void _setTextFieldDate(DateTime date) {
